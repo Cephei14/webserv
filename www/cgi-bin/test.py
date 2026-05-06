9b28a4f219eb3cc52eb9563dacce0c552034fb05
cephei14 <rafik.dhaibi@gmail.com>
2026-05-07 01:57:27 +0900

    updated

--- www/cgi-bin/test.py
+++ www/cgi-bin/test.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 import os
+import sys
+
+body = sys.stdin.read()
 
 print("Status: 200")
 print("Content-Type: text/plain")
 print()
 print("method=" + os.environ.get("REQUEST_METHOD", ""))
+print("query=" + os.environ.get("QUERY_STRING", ""))
+print("len=" + os.environ.get("CONTENT_LENGTH", ""))
+print("body=" + body)
 print("CGI ended!")
